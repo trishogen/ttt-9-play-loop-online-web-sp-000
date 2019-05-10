0977f7f89e45c9ec94eec3ede32974b43d331bb8
Trish Ogen <patriciaogen@gmail.com>
2019-05-10 02:03:33 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,13 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  turns_taken = 0
+  while turns_taken < 9 do
+    turn(board)
+    turns_taken += 1
+  end
+end  
+    
+  
